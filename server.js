79a9d8e88122ee1c2c40883c48273d7a7bce99c9
Yashasvi Singh <yashasvisingh2873@gmail.com>
2022-11-04 22:12:11 +0530

    city array made with prev changes

--- server.js
+++ server.js
@@ -12,30 +12,153 @@ const ejs = require("ejs");
 app.get("/", (req, res) => {
   res.render("monopoly_board");
 });
-
+const go="go",city="city",company="company",chest="chest",chance="chance",jail="jail",go_jail="go_jail",park="park";
+let places=[
+  {
+  cell:0,
+  name:go,
+  price:0
+  },
+  {
+  cell:1,
+  name:city,
+  price:60
+  },
+  {
+    cell:2,
+    name:chest,
+    price:0
+  },
+  {
+    cell:3,
+    name:city,
+    price:100
+  },
+  {
+    cell:4,
+    name:chance,
+    price:0
+  },
+  {
+    cell:5,
+    name:city,
+    price:120
+  },
+  {
+    cell:6,
+    name:jail,
+    price:0
+  },
+  {
+    cell:7,
+    name:city,
+    price:140
+  },
+  {
+    cell:8,
+    name:company,
+    price:150
+  },
+  {
+    cell:9,
+    name:city,
+    price:160
+  },
+  {
+    cell:10,
+    name:chest,
+    price:0
+  },
+  {
+    cell:11,
+    name:city,
+    price:200
+  },
+  {
+    cell:12,
+    name:park,
+    price:0
+  },
+  {
+    cell:13,
+    name:city,
+    price:220
+  },
+  {
+    cell:14,
+    name:chance,
+    price:0
+  },
+  {
+    cell:15,
+    name:city,
+    price:240
+  },
+  {
+    cell:16,
+    name:company,
+    price:0
+  },
+  {
+    cell:17,
+    name:city,
+    price:280
+  },
+  {
+    cell:18,
+    name:go_jail,
+    price:0
+  },
+  {
+    cell:19,
+    name:city,
+    price:300
+  },
+  {
+    cell:20,
+    name:chest,
+    price:0
+  },
+  {
+    cell:21,
+    name:city,
+    price:320
+  },
+  {
+    cell:22,
+    name:chance,
+    price:0
+  },
+  {
+    cell:23,
+    name:city,
+    price:400
+  }
+]
 let clients = 0;
-let player_data = {
-  socket_id: 0,
-  name: 0,
-  curr_pos: 0,
-};
 let players = [{
   socket_id: 0,
   name: 1,
   curr_pos: 0,
+  money:300
 },
 {
   socket_id: 0,
   name: 2,
   curr_pos: 0,
-},{
+  money:300
+},
+{
   socket_id: 0,
   name: 3,
   curr_pos: 0,
-},{
+  money:300
+},
+{
   socket_id: 0,
   name: 4,
   curr_pos: 0,
+  money:300
 }
 ];
 var roomno = 1;
